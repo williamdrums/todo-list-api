c41513f8c4d8d5c9b3169aff293089333f562ae3
William Nascimento <william.nascimento@capgemini.com>
2021-09-24 11:08:06 -0300

    adicionando connection db oracle

--- src/main/java/com/wlnascimento/todolist/domain/model/Todo.java
+++ src/main/java/com/wlnascimento/todolist/domain/model/Todo.java
@@ -2,10 +2,7 @@ package com.wlnascimento.todolist.domain.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -14,6 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Builder
@@ -25,7 +23,7 @@ public class Todo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Size(max = 60)
